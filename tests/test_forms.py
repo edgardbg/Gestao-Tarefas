b7c4b3c06230e776c4f50d6963093823427bf95c
Edgard Butze Grüdtner <91330665+edgardbg@users.noreply.github.com>
2025-04-13 14:02:36 -0300

    Ajustes no test_registration_form

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,21 +1,26 @@
 import pytest
+from todo_project import app
 from todo_project.forms import RegistrationForm, LoginForm, UpdateUserInfoForm, UpdateUserPassword, TaskForm, UpdateTaskForm
 
 def test_registration_form():
-    form = RegistrationForm(username='testuser', password='password', confirm_password='password')
-    assert form.validate() is True
+    with app.app_context():
+        form = RegistrationForm(username='testuser', password='password', confirm_password='password')
+        assert form.validate() is True
 
 def test_login_form():
-    form = LoginForm(username='testuser', password='password')
-    assert form.validate() is True
+    with app.app_context():
+        form = LoginForm(username='testuser', password='password')
+        assert form.validate() is True
 
 def test_update_user_info_form():
-    form = UpdateUserInfoForm(username='testuser')
-    assert form.validate() is True
+    with app.app_context():
+        form = UpdateUserInfoForm(username='testuser')
+        assert form.validate() is True
 
 def test_update_user_password_form():
-    form = UpdateUserPassword(old_password='password', new_password='newpassword')
-    assert form.validate() is True
+    with app.app_context():
+        form = UpdateUserPassword(old_password='password', new_password='newpassword')
+        assert form.validate() is True
 
 def test_task_form():
     form = TaskForm(task_name='Test Task')
