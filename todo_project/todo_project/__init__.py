b59dffb5f7b983b9eea222abbd81f3ff29a85b06
Edgard Butze Grüdtner <91330665+edgardbg@users.noreply.github.com>
2025-04-13 15:47:07 -0300

    Correção após diagnóstico do Bandit

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -5,7 +6,7 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
